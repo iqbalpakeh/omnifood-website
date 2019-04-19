39710f2e42580dda9948687204b6320444a5edf2
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-19 08:45:52 +0700

    Animate features section

--- resources/js/script.js
+++ resources/js/script.js
@@ -76,4 +76,16 @@ $(document).ready(function() {
         }
       }
     });
+
+  /**
+   * Animations on scroll
+   */
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
 });
